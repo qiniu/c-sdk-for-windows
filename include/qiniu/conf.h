f5106c83dfdcdbef412e1b0b18ad97eb17066922
jemygraw <jemygraw@gmail.com>
2016-05-08 13:17:56 +0800

    update the dll and lib to the latest sdk

--- include/qiniu/conf.h
+++ include/qiniu/conf.h
@@ -10,11 +10,27 @@
 #ifndef QINIU_CONF_H
 #define QINIU_CONF_H
 
-extern const char* QINIU_ACCESS_KEY;
-extern const char* QINIU_SECRET_KEY;
-
-extern const char* QINIU_RS_HOST;
-extern const char* QINIU_UP_HOST;
-
+#ifdef __cplusplus
+extern "C"
+{
 #endif
 
+#if defined(USING_QINIU_LIBRARY_DLL)
+    #define QINIU_DLLIMPORT __declspec(dllimport)
+#else
+    #define QINIU_DLLIMPORT 
+#endif
+
+
+QINIU_DLLIMPORT extern const char* QINIU_ACCESS_KEY;
+QINIU_DLLIMPORT extern const char* QINIU_SECRET_KEY;
+
+QINIU_DLLIMPORT extern const char* QINIU_RS_HOST;
+QINIU_DLLIMPORT extern const char* QINIU_UP_HOST;
+QINIU_DLLIMPORT extern const char* QINIU_API_HOST;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
